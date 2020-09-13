5c7d1855e1cf42256dd4f99477bb53ce1b05f67a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-13 14:52:57 +0000

    Deploy zhongyangxun/pocket-book to github.com/zhongyangxun/pocket-book.git:gh-pages

---
+++ precache-manifest.6c18e70e389af2a94262db0ad1bdf5d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b591d525acc94f83e19c80f3b7831f9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d237ee19b7fb5d0286ff",
+    "url": "/static/css/2.2f73783b.chunk.css"
+  },
+  {
+    "revision": "81ec9d38bf9c81ea59bc",
+    "url": "/static/css/main.db43efbc.chunk.css"
+  },
+  {
+    "revision": "d237ee19b7fb5d0286ff",
+    "url": "/static/js/2.46c0c285.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.46c0c285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81ec9d38bf9c81ea59bc",
+    "url": "/static/js/main.f2fb69cb.chunk.js"
+  },
+  {
+    "revision": "7fa5e20d7e7eb4f07978",
+    "url": "/static/js/runtime-main.c2a89994.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pocket-book/precache-manifest.c6d530fefe094cebaaecee23670c134f.js"
+  "/precache-manifest.6c18e70e389af2a94262db0ad1bdf5d3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pocket-book/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
