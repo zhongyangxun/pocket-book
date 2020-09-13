4535a74ced9233afed50e1f8960924cfbc4ba25b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-13 10:15:42 +0000

    Deploy zhongyangxun/pocket-book to github.com/zhongyangxun/pocket-book.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.028b88c0359814055a4ea87b8f89baa8.js"
+  "/pocket-book/build/precache-manifest.67fde37c3e6601441a4806c174565b3d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pocket-book/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
